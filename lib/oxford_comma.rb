1e724b813998539715e99da0736785d471f4b44e
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-10 12:00:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-"#{array.join(", ")}"
+#{}"#{array.join(", ")}"
+new_array = nil
+  array.each_with_index.collect do |element, index|
+    if index < array.length
+      new_array << "#{element},"
+    else
+      new_array << "and #{element}"
+    end  
+  end
 end
