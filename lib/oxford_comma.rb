53b93ae8539a575dd645851c0bc27f569636c41a
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-10 12:14:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,13 @@ def oxford_comma(array)
 #{}"#{array.join(", ")}"
 new_array = ""
   array.each_with_index.collect do |element, index|
-    if index < array.length - 1
+    if index < array.length - 1 && array.length = 1
+      new_array << element
+    elsif index < array.length - 1
       new_array << "#{element}, "
     else
       new_array << "and #{element}"
-    end
+    end  
   end
 new_array
 end
